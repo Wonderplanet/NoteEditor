8290bc5ad03207455d4432199e959090cf6e4914
weiping <toh@wonderpla.net>
2019-04-22 12:15:22 +0900

    beatをshiftする

---
+++ Assets/Scripts/Presenter/Toolstrip/BeatShiftPresenter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace NoteEditor.Presenter
+{
+    using Model;
+    using Notes;
+    public class BeatShiftPresenter : MonoBehaviour
+    {
+        public void ShiftNotesToLeft()
+        {
+            var notes = EditData.Notes.Values.ToList();
+            foreach(var noteObj in notes)
+            {
+                var position = noteObj.note.position;
+                var note = noteObj.note;
+                EditData.Notes.Remove(position);
+                position.num -= position.LPB;
+                note.position = position;
+                var noteObject = new NoteObject();
+                noteObject.SetState(note);
+                noteObject.Init();
+                EditData.Notes[position] = noteObj;
+            }
+        }
+
+        public void ShiftNotesToRight()
+        {
+            var notes = EditData.Notes.Values.ToList();
+            foreach (var noteObj in notes)
+            {
+                var position = noteObj.note.position;
+                var note = noteObj.note;
+                EditData.Notes.Remove(position);
+                position.num += position.LPB;
+                note.position = position;
+                var noteObject = new NoteObject();
+                noteObject.SetState(note);
+                noteObject.Init();
+                EditData.Notes[position] = noteObj;
+            }
+        }
+    }
+}
